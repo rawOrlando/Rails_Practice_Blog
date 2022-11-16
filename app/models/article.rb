a0fb39d4697dc6b3272500cab2cd0078ca466817
Ryan Orlando <rynorlando@gmail.com>
2022-11-16 10:23:22 -0500

    Add mild validation to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
-
+	# First one writen like a method to remind me what 
+	# it is. Secound way of writing it is standard in ruby.
+	validates(:title, presence: true, length: {minimum: 6, maximum: 100})
+	validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
