a6b4534713686769fa147913478753683dd65678
Ryan Orlando <rynorlando@gmail.com>
2023-01-25 13:33:43 -0500

    Add sign up page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
+
+  def create
+    #byebug
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+      redirect_to articles_path #todo change
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
