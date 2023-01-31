dd69e1cf14bd4c225b0ef9e7b0254626b0e19649
Ryan Orlando <rynorlando@gmail.com>
2023-01-31 11:25:27 -0500

    Add User Show Page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def gravatar_for(user, options = { size: 80})
+    email = user.email.downcase
+    hash = Digest::MD5.hexdigest(email)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
 end
