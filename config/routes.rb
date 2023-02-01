f447bd2c04bcfb128a4f25902fa822234d3ae7e8
Ryan Orlando <rynorlando@gmail.com>
2023-02-01 10:07:40 -0500

    Add login form

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destory]
   get "signup", to: 'users#new'
   resources :users, except: [:new]
+  get "login", to: 'sessions#new'
+  post "login", to: 'sessions#create'
+  delete "login", to: 'sessions#destroy'
+
 end
