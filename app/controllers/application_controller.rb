e0b5cc9934baa24a14e642bce635dd63307fea57
Ryan Orlando <rynorlando@gmail.com>
2023-02-16 10:57:59 -0500

    Add categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,11 @@ class ApplicationController < ActionController::Base
       redirect_to login_path
     end
   end
+
+  def require_admin
+    if !logged_in? || !current_user.admin
+      flash[:alert] = "You must be an admin to perform that action"
+      redirect_to login_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   get "login", to: 'sessions#new'
   post "login", to: 'sessions#create'
   get "logout", to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
 
 end
